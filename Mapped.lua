ca5e19bc3021be3c6b49b3c94f3fc1db3e02da98
Bl00DGuY <themike0785@gmail.com>
2013-10-18 15:06:04 -0400

    UI & Basic Function

--- Mapped.lua
+++ Mapped.lua
@@ -2,6 +2,7 @@
 local AceGUI = LibStub("AceGUI-3.0")
 local currentZone = GetZoneText();
 local currentSubZone = GetSubZoneText();
+local devDebug = false;
 
 function Mapped:OnInitialize()
     -- Called when the addon is loaded
@@ -9,22 +10,101 @@ end
 
 function Mapped:OnEnable()
     self:Print("Version 0.0.0.1 loaded.")
-	self:RegisterEvent("ZONE_CHANGED")
+	if (devDebug == true) then
+	self:Print("Debug Enabled.")
+	end
 end
 
-function Mapped:ZONE_CHANGED()
-	self:Print(GetZoneText())
+-- Main Frame
+local f = CreateFrame("Frame","MappedPanel",UIParent)
+f:SetFrameStrata("BACKGROUND")
+f:SetWidth(512) -- Set these to whatever height/width is needed 
+f:SetHeight(256) -- for your Texture
+
+local t = f:CreateTexture(nil,"BACKGROUND")
+t:SetTexture("Interface\\Addons\\Mapped\\Texture\\Mainframe.blp")
+t:SetAllPoints(f)
+f.texture = t
+
+f:SetPoint("CENTER",0,0)
+f:Show()
+
+-- Frame Movement 
+f:EnableMouse(true)
+f:SetMovable(true)
+f:SetScript("OnMouseDown", function(self, button)
+  if button == "LeftButton" and not self.isMoving then
+   self:StartMoving();
+   self.isMoving = true;
+  end
+end)
+f:SetScript("OnMouseUp", function(self, button)
+  if button == "LeftButton" and self.isMoving then
+   self:StopMovingOrSizing();
+   self.isMoving = false;
+  end
+end)
+f.Text = MappedPanel:CreateFontString(nil, "LOW")
+f.Text:Point("CENTER", 0, 0)
+f:SetAlpha(.75);
+
+-- Version Text
+local VerTxt = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+VerTxt:SetPoint("BOTTOM", 230, 5)
+VerTxt:SetText(GetZoneText())
+VerTxt:SetFont(DEFAULT, 20, "OUTLINE, MONOCHROME")
+VerTxt:SetText("0.0.0.1a")
+
+-- Current Location Text
+local CurrentLocationTxt = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+CurrentLocationTxt:SetPoint("TOP", 80, -20)
+CurrentLocationTxt:SetText(GetZoneText())
+CurrentLocationTxt:SetFont("Interface\\Addons\\Mapped\\Font\\homespun.ttf", 20, "OUTLINE, MONOCHROME")
+
+-- Current Sub_Location Text
+local CurrentSubLocationTxt = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+CurrentSubLocationTxt:SetPoint("TOP", 80, -40)
+CurrentSubLocationTxt:SetText(GetSubZoneText())
+CurrentSubLocationTxt:SetFont("Interface\\Addons\\Mapped\\Font\\homespun.ttf", 16, "OUTLINE, MONOCHROME")
+
+f:RegisterEvent("ZONE_CHANGED")
+local function eventHandler(self, ZONE_CHANGED, ...)
+ if (GetSubZoneText() == "") then
+			CurrentLocationTxt:SetText(GetZoneText())
+			CurrentSubLocationTxt:SetText(nil)
+		else
+			CurrentLocationTxt:SetText(GetZoneText())
+			CurrentSubLocationTxt:SetText(GetSubZoneText())
+		end	
 end
+f:SetScript("OnEvent", eventHandler);
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+local function eventHandler(self, ZONE_CHANGED_NEW_AREA, ...)
+ if (GetSubZoneText() == "") then
+			CurrentLocationTxt:SetText(GetZoneText())
+			CurrentSubLocationTxt:SetText(nil)
+		else
+			CurrentLocationTxt:SetText(GetZoneText())
+			CurrentSubLocationTxt:SetText(GetSubZoneText())
+		end	
+end
+f:SetScript("OnEvent", eventHandler);
 
-local mainFrame = AceGUI:Create("Frame")
-mainFrame:SetTitle("Mapped")
-mainFrame:SetStatusText("Mapped 0.0.0.1")
+----- DEBUG ---------------------------------------------------------------------------
+if (devDebug == true) then
 
-local labelCurZone = AceGUI:Create("Label")
-labelCurZone:SetFont("Font\\homespun.ttf", 12, "OUTLINE")
-labelCurZone:SetText(currentZone)
-mainFrame:AddChild(labelCurZone)
-
-local labelCurSubZone = AceGUI:Create("Label")
-labelCurSubZone:SetText(currentSubZone)
-mainFrame:AddChild(labelCurSubZone)
+	-- Print Current Zone--------------------------------------
+	function Mapped:ZONE_CHANGED()
+		if (GetSubZoneText() == "") then
+			self:Print("[Debug]",GetZoneText())
+			CurrentLocationTxt:SetText(GetZoneText())
+			CurrentSubLocationTxt:SetText(GetSubZoneText())
+		else
+			self:Print("[Debug]",GetZoneText()," (",GetSubZoneText(),")")
+			CurrentLocationTxt:SetText(GetZoneText())
+			CurrentSubLocationTxt:SetText(GetSubZoneText())
+		end	
+	end
+	-----------------------------------------------------------
+	
+end
