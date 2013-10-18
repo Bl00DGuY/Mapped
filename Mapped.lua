715fc41551e9d1f565c637e73657bd4bd86efd1f
Bl00DGuY <themike0785@gmail.com>
2013-10-18 16:37:37 -0400

    Added Minimap Icon

--- Mapped.lua
+++ Mapped.lua
@@ -29,6 +29,38 @@ f.texture = t
 f:SetPoint("CENTER",0,0)
 f:Show()
 
+	-- Button (Main Frame)
+	local button = CreateFrame("Button", nil, f)
+	button:SetPoint("Bottom", f, "Bottom", 0, 0)
+	button:SetWidth(80)
+	button:SetHeight(32)
+	
+	button:SetText("Close")
+	button:SetNormalFontObject("GameFontNormal")
+	
+	local ntex = button:CreateTexture()
+	ntex:SetTexture("Interface\\Addons\\Mapped\\Texture\\ButtonHide_UP.blp")
+	ntex:SetTexCoord(0, 0.625, 0, 0.6875)
+	ntex:SetAllPoints()	
+	button:SetNormalTexture(ntex)
+	
+	local htex = button:CreateTexture()
+	htex:SetTexture("Interface\\Addons\\Mapped\\Texture\\ButtonHide_HIGH.blp")
+	htex:SetTexCoord(0, 0.625, 0, 0.6875)
+	htex:SetAllPoints()
+	button:SetHighlightTexture(htex)
+	
+	local ptex = button:CreateTexture()
+	ptex:SetTexture("Interface\\Addons\\Mapped\\Texture\\ButtonHide_DOWN.blp")
+	ptex:SetTexCoord(0, 0.625, 0, 0.6875)
+	ptex:SetAllPoints()
+	button:SetPushedTexture(ptex)
+	
+	button:SetScript("OnClick", function(self, arg1)
+    f:Hide()
+	end)
+	button:Click(f:Hide())
+
 -- Frame Movement 
 f:EnableMouse(true)
 f:SetMovable(true)
@@ -49,11 +81,11 @@ f.Text:Point("CENTER", 0, 0)
 f:SetAlpha(.75);
 
 -- Version Text
-local VerTxt = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-VerTxt:SetPoint("BOTTOM", 230, 5)
-VerTxt:SetText(GetZoneText())
-VerTxt:SetFont(DEFAULT, 20, "OUTLINE, MONOCHROME")
-VerTxt:SetText("0.0.0.1a")
+--local VerTxt = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+--VerTxt:SetPoint("BOTTOM", 230, 5)
+--VerTxt:SetText(GetZoneText())
+--VerTxt:SetFont(DEFAULT, 20, "OUTLINE, MONOCHROME")
+--VerTxt:SetText("0.0.0.1a")
 
 -- Current Location Text
 local CurrentLocationTxt = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
@@ -89,6 +121,48 @@ local function eventHandler(self, ZONE_CHANGED_NEW_AREA, ...)
 		end	
 end
 f:SetScript("OnEvent", eventHandler);
+
+-- Minimap Button
+
+local db; -- File-global handle to the Database
+local defaults = {
+	profile = {
+		LDBIconStorage = {}, -- LibDBIcon storage
+	},
+};
+
+local ldbObject = {
+	type = "launcher",
+	icon = "Interface\\ICONS\\spell_nature_bloodlust",
+--This is the icon used. Any .blp or .tga file is a valid icon.
+--This path is ALWAYS relative to the World of Warcraft
+--root (ie, "C:\Program Files\World of Warcraft" for
+--Windows and "/Applications/World of Warcraft" for Mac)
+	label = "AddonName",
+	OnClick = function(self, button)
+		f:Show()
+	end,
+	OnTooltipShow = function(tooltip)
+		tooltip:AddLine("Mapped");
+--Add text here. The first line is ALWAYS a "header" type.
+--It will appear slightly larger than subsequent lines of text
+	end,
+};
+
+function updateDB(self, event, database)
+	db = database.profile;
+	LibStub("LibDBIcon-1.0"):Refresh("AddonLDBObjectName", db.LDBIconStorage);
+end
+
+local vars = LibStub("AceDB-3.0"):New("AddonSavedVarStorage", defaults);
+vars:RegisterCallback("OnProfileChanged", updateDB);
+vars:RegisterCallback("OnProfileCopied", updateDB);
+vars:RegisterCallback("OnProfileReset", updateDB);
+db = vars.profile;
+
+LibStub("LibDataBroker-1.1"):NewDataObject("AddonLDBObjectName", ldbObject);
+LibStub("LibDBIcon-1.0"):Register("AddonLDBObjectName", ldbObject, db.LDBIconStorage);
+
 
 ----- DEBUG ---------------------------------------------------------------------------
 if (devDebug == true) then
