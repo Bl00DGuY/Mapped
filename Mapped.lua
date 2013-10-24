cf849e700dd50d114f4d7f77a291e1f8c2d98c19
Bl00DGuY <themike0785@gmail.com>
2013-10-24 17:47:40 -0400

    UI Tweak + GameToolTip Logic + Instance Info

--- Mapped.lua
+++ Mapped.lua
@@ -10,14 +10,14 @@ function Mapped:OnInitialize()
 end
 
 function Mapped:OnEnable()
-    self:Print("Version 0.0.0.1a loaded.")
+    self:Print("Version 1.0.0.0 loaded.")
 end
 
 -- Main Frame
 Mapped.Main = CreateFrame("Frame", Mapped.Main, UIParent)
 Mapped.Main:SetPoint("BOTTOM", "UIParent", "TOP")
 Mapped.Main:SetFrameStrata("LOW")
-Mapped.Main:SetHeight(80)
+Mapped.Main:SetHeight(50)
 Mapped.Main:SetBackdrop({
 	bgFile = "Interface/Tooltips/ChatBubble-Background",
 	edgeFile = "Interface/Tooltips/ChatBubble-BackDrop",
@@ -25,7 +25,6 @@ Mapped.Main:SetBackdrop({
 	insets = { left = 32, right = 32, top = 32, bottom = 32 }
 })
 Mapped.Main:SetBackdropColor(0,0,0, 1)
-Mapped.Main:SetScript("OnShow", Swatter.ErrorShow)
 Mapped.Main:SetMovable(true)
 Mapped.Main:EnableMouse(true)
 Mapped.Main:SetClampedToScreen(true)
@@ -45,7 +44,6 @@ Mapped.Main.Prof:SetBackdrop({
 	insets = { left = 32, right = 32, top = 32, bottom = 32 }
 })
 Mapped.Main.Prof:SetBackdropColor(0,0,0, 1)
-Mapped.Main.Prof:SetScript("OnShow", Swatter.ErrorShow)
 Mapped.Main.Prof:SetMovable(true)
 Mapped.Main.Prof:EnableMouse(true)
 Mapped.Main.Prof:SetClampedToScreen(true)
@@ -55,20 +53,23 @@ Mapped.Main.Done = CreateFrame("Button", "", Mapped.Main, "OptionsButtonTemplate
 Mapped.Main.Done:SetText("Close")
 Mapped.Main.Done:SetPoint("BOTTOMLEFT", Mapped.Main, "BOTTOMLEFT", 10, 5)
 Mapped.Main.Done:SetScript("OnClick", function() Mapped.Main:Hide() end)
+Mapped.Main.Done:Hide()
 	-- Config Button
 Mapped.Main.Config = CreateFrame("Button", "", Mapped.Main, "OptionsButtonTemplate")
 Mapped.Main.Config:SetText("Professions")
 Mapped.Main.Config:SetPoint("BOTTOMRIGHT", Mapped.Main, "BOTTOMRIGHT", -10, 5)
 Mapped.Main.Config:SetScript("OnClick", function() InstanceFrameControl() end)
+Mapped.Main.Config:Hide()
 	-- Main Version Text
 Mapped.Main.Version = Mapped.Main:CreateFontString(nil, "LOW")
 Mapped.Main.Version:Point("TOPRIGHT", 0, -5)
 Mapped.Main.Version:SetFont("Fonts\\MORPHEUS.ttf", 10, "OUTLINE")
-Mapped.Main.Version:SetText("Mapped 0.1.0.0b")
+Mapped.Main.Version:SetText("Mapped 1.0.0.0")
 	-- Main Frame Text
 Mapped.Main.Text = Mapped.Main:CreateFontString(nil, "LOW")
-Mapped.Main.Text:Point("CENTER", 0, 0)
+Mapped.Main.Text:Point("BOTTOM", 0, 0)
 Mapped.Main.Text:SetFont("Interface\\Addons\\Mapped\\Font\\homespun.ttf", 20, "THICKOUTLINE")
+Mapped.Main:SetScript("OnLeave", function() GameTooltip:Hide() end)
 	-- Main Level Text
 Mapped.Main.LevelText = Mapped.Main:CreateFontString(nil, "LOW")
 Mapped.Main.LevelText:Point("TOP", 0, 0)
@@ -86,6 +87,52 @@ Mapped.Main.Prof.FishText = Mapped.Main.Prof:CreateFontString(nil, "LOW")
 Mapped.Main.Prof.FishText:Point("TOPLEFT", 10, -30)
 Mapped.Main.Prof.FishText:SetFont("Interface\\Addons\\Mapped\\Font\\homespun.ttf", 18, "OUTLINE")
 	-- Main Frame OnUpdate Script
+
+local function CurrentDungCoords(zone)
+	local z, x, y = "", 0, 0;
+	local dcoords
+	
+	if tourist:IsInstance(zone) then
+		z, x, y = tourist:GetEntrancePortalLocation(zone);
+	end
+	
+	if z == nil then
+		dcoords = ""
+	else
+		x = tonumber(roundNum(x, 0))
+		y = tonumber(roundNum(y, 0))		
+		dcoords = string.format(" (%d:%d)", x, y)
+	end
+
+	return dcoords
+end
+	
+local function CurrentDungZones(zone)
+
+	local low, high = tourist:GetLevel(zone)
+	local r, g, b = tourist:GetLevelColor(zone)
+	local groupSize = tourist:GetInstanceGroupSize(zone)
+	local altGroupSize = tourist:GetInstanceAltGroupSize(zone)
+	local groupSizeStyle = (groupSize > 0 and string.format("|cFFFFFF00|r (%d", groupSize) or "")
+	local altGroupSizeStyle = (altGroupSize > 0 and string.format("|cFFFFFF00|r/%d", altGroupSize) or "")
+	
+	GameTooltip:AddLine(string.format("|cff%02x%02x%02x %s", r*255, g*255, b*255, zone) .. " " .. string.format("|cff%02x%02x%02x (%d-%d) |r", r*255, g*255, b*255, low, high)
+	..groupSize .. "M"
+	..CurrentDungCoords(zone)
+	)
+
+end
+
+local function CurrentRecZones(zone)
+
+	local low, high = tourist:GetLevel(zone)
+	local r, g, b = tourist:GetLevelColor(zone)
+	local zContinent = tourist:GetContinent(zone)
+	
+	GameTooltip:AddDoubleLine(zone .. " " .. string.format("|cff%02xff00%s|r", continent == zContinent and 0 or 255, zContinent) ..(" |cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255,(low == high and low or ("(%d-%d)"):format(low, high))));
+
+end
+	
 Mapped.Main:SetScript("OnUpdate", function(self,event,...)
 	local subZoneText = GetMinimapZoneText() or ""
 	local zoneText = GetRealZoneText() or UNKNOWN;
@@ -93,6 +140,41 @@ Mapped.Main:SetScript("OnUpdate", function(self,event,...)
 	local low, high = tourist:GetLevel(zoneText)
 	local r, g, b = tourist:GetLevelColor(zoneText)
 	local x, y = GetPlayerMapPosition("player")
+	local curPos = (zoneText.." ") or "";
+	local low,high = tourist:GetBattlePetLevel(zoneText)
+	local minFish = tourist:GetFishingLevel(zoneText)
+	
+	-- GameToolTip Logic
+	if MouseIsOver(self) then
+	    GameTooltip:ClearLines()
+        GameTooltip:SetOwner(self, "ANCHOR_BOTTOM") 
+		GameTooltip:AddDoubleLine(HOME.." :", GetBindLocation(), 1, 1, 1, selectioncolor)
+		
+		if low ~= nil or high ~= nil then
+			GameTooltip:AddLine(" ")
+			GameTooltip:AddDoubleLine("Battle Pet level".. " :", low == high and low or string.format("%d-%d", low, high), 1, 1, 1, selectioncolor)
+		end
+		
+		GameTooltip:AddDoubleLine("Fishing" .. " : ", minFish, 1, 1, 1, selectioncolor)
+		
+		GameTooltip:AddLine(" ")
+		GameTooltip:AddLine("Recommended Zones :", 1, 1, 1, selectioncolor)
+	
+		for zone in tourist:IterateRecommendedZones() do
+			CurrentRecZones(zone);
+		end	
+		
+		if tourist:DoesZoneHaveInstances(zoneText) then 
+			GameTooltip:AddLine(" ")
+			GameTooltip:AddLine("Instance(s): ", 1, 1, 1, selectioncolor)	
+		
+			for zone in tourist:IterateZoneInstances(zoneText) do
+				CurrentDungZones(zone)
+			end
+		end		
+		
+		GameTooltip:Show()
+	end
 	
 		-- Ensure the Main/Prof frame is never under 300 in width
 	if (self.Text:GetStringWidth() + 18) < 300 then
@@ -132,7 +214,7 @@ Mapped.Main:SetScript("OnUpdate", function(self,event,...)
 		
 	local minFish = tourist:GetFishingLevel(zoneText)
 		if minFish then
-			Mapped.Main.Prof.FishText:SetText("Fishing: " .. minFish, 1, 1, 1, selectioncolor)
+			Mapped.Main.Prof.FishText:SetText(PROFESSIONS_FISHING .. ": " .. minFish, 1, 1, 1, selectioncolor)
 		end
 	
 end)
